4e2003d86561187cad4b47f9d759907271a6358e
Shrirang <shrirang.sapate95@gmail.com>
2024-09-20 13:09:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class GeminiLLM(LLM):
     
     def __init__(self, model_name: str):
         # super().__init__()
-        self.model_name = model_name
+        self.model_name = "gemini-1.5-flash"
         self.model = genai.GenerativeModel(model_name=model_name)
     
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
