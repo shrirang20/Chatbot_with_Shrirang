033d0ed7c5e6562bf7a69bb482815fb4e9583637
Shrirang <shrirang.sapate95@gmail.com>
2024-09-20 12:56:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-from database import db
-import datetime
+# from database import db
+# import datetime
 
-class Prompt(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    prompt = db.Column(db.String, nullable=False)
-    answer = db.Column(db.String, nullable=False)
-    time = db.Column(db.DateTime, nullable=False, default = datetime.datetime.utcnow)
+# class Prompt(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     prompt = db.Column(db.String, nullable=False)
+#     answer = db.Column(db.String, nullable=False)
+#     time = db.Column(db.DateTime, nullable=False, default = datetime.datetime.utcnow)
